8d3acffe3d3eb6497f2eb30ac2b37541e491e8aa
ainuo5213 <1660998482@qq.com>
2022-06-03 22:33:51 +0800

    build: 打包页面

---
+++ prod/js/24.0dfe4f2c.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvue_strategy_front_backend"]=self["webpackChunkvue_strategy_front_backend"]||[]).push([[24],{8024:function(e,n,r){r.r(n),r.d(n,{default:function(){return s}});var t=r(3396);function u(e,n,r,u,a,c){return(0,t.wg)(),(0,t.iD)("div",null,"这是home")}var a={},c=r(89);const f=(0,c.Z)(a,[["render",u]]);var s=f}}]);
+//# sourceMappingURL=24.0dfe4f2c.js.map
